c5bca0c070e40f27e21ff10529ba30679b2ca9c0
togger <roger.ikari@gmail.com>
2017-12-03 21:51:20 -0200

    FrontEnd MVC adicionado

---
+++ NextlibRestBackend/NextLibRestFrontEndMVC/Controllers/ClienteController.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using Newtonsoft.Json;
+using NextLibRestFrontEndMVC.Models;
+
+namespace NextLibRestFrontEndMVC.Controllers
+{
+    public class ClienteController : Controller
+    {
+        // GET: Cliente
+        public ActionResult Index()
+        {
+            List<ClienteViewModel> clientes = null;
+
+            using (var cliente = new HttpClient())
+            {
+                cliente.BaseAddress = new Uri("http://localhost:2379/api/");
+                
+                var responseTask = cliente.GetAsync("clientes");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsStringAsync();
+                    readTask.Wait();
+
+                    clientes = JsonConvert.DeserializeObject<List<ClienteViewModel>>(readTask.Result);
+                }
+            }
+
+            return View(clientes);
+        }
+
+        // GET: Cliente/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Cliente/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Cliente/Edit/5
+        public ActionResult Edit(string id)
+        {
+            ClienteViewModel cliente = null;
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:2379/api/");
+
+                var responseTask = client.GetAsync("clientes/" + id);
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsStringAsync();
+                    readTask.Wait();
+
+                    cliente = JsonConvert.DeserializeObject<ClienteViewModel>(readTask.Result);
+                }
+            }
+
+            return View(cliente);
+        }
+
+        // POST: Cliente/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(string id, ClienteViewModel cliente)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Cliente/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Cliente/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ NextlibRestBackend/NextLibRestFrontEndMVC/Models/ClienteViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NextLibRestFrontEndMVC.Models
+{
+    public class ClienteViewModel
+    {
+        public object Id { get; set; }
+        public string OuterId { get; set; }
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public string Nascimento { get; set; }
+        public string Telefone { get; set; }
+    }
+}
